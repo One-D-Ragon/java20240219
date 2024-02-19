1ea265418e8bd3299c3f422377a67b0ea7448f3e
1dragon <dnjsdyd159@naver.com>
2024-02-19 12:41:08 +0900

    처음 만들어본 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,12 @@
+public class Main3 {
+    // main
+    public static void main(String[] args) {
+
+        // sout
+        System.out.println("오늘 점심 뭐먹지?");
+        System.out.println("오늘 저녁 뭐먹지?");
+        System.out.println("내일 아침 뭐먹지?");
+        System.out.println("내일 점심 뭐먹지?");
+        System.out.println("내일 저녁 뭐먹지?");
+    }
+}
